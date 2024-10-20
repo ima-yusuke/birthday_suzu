961ed92af86ffbe26c42df43a3b05e78920e195c
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-10-20 21:42:01 +0900

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+<x-template title="happy birthday Suzu">
+    <section class="min-h-screen w-full relative flex justify-center items-center">
+        <div class="min-h-screen w-full bg-gray-400 absolute z-20 opacity-50"></div>
+        <h1 class="text-center text-xl font-bold absolute z-30 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">Happy Birthday SUZU</h1>
+        <img src="{{ asset('storage/img/pic-02.jpeg') }}" alt="pic-02" class="object-cover min-h-screen w-full">
+    </section>
+    <img src="{{ asset('storage/img/pic-01.jpeg') }}" alt="pic-01" class="object-cover min-h-screen w-full">
+</x-template>
