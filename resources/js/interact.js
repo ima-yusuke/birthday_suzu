9ecbeb8b3ff009994c911f133a43879c2f1402a9
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-11-30 21:31:01 +0900

    add：全ての要素がドロップゾーンにドロップされたかどうかを確認する関数を実装

--- resources/js/interact.js
+++ resources/js/interact.js
@@ -1,5 +1,7 @@
 import interact from 'interactjs';
 
+let dragElements = document.querySelectorAll('.yes-drop');
+
 // ドラッグ時の動作を定義
 function dragMoveListener(event) {
     var target = event.target;
@@ -15,15 +17,17 @@ function dragMoveListener(event) {
     target.setAttribute('data-y', y);
 }
 
-// ドロップゾーンの設定
-interact('.dropzone').dropzone({
-    accept: '.yes-drop',
+// 野菜ドロップゾーンの設定
+interact('.vegetable_dropzone').dropzone({
+    accept: '.vegetable',
     overlap: 0.75,
 
     // ドラッグが開始されたとき
     // ドロップゾーンがアクティブであることを視覚的にユーザーに示す
     ondropactivate: function (event) {
         event.target.classList.add('drop-active');
+        event.relatedTarget.classList.remove('success');
+
     },
     // ドラッグ中の要素がドロップゾーンに入ったとき
     // ドラッグ可能な要素がドロップゾーンに到達したことを視覚的に示す
@@ -53,8 +57,51 @@ interact('.dropzone').dropzone({
             event.relatedTarget.setAttribute('data-x', 0);
             event.relatedTarget.setAttribute('data-y', 0);
         }
+        CheckAmount();
+    }
+});
 
+// フルーツドロップゾーンの設定
+interact('.fruit_dropzone').dropzone({
+    accept: '.fruit',
+    overlap: 0.75,
+
+    // ドラッグが開始されたとき
+    // ドロップゾーンがアクティブであることを視覚的にユーザーに示す
+    ondropactivate: function (event) {
+        event.target.classList.add('drop-active');
         event.relatedTarget.classList.remove('success');
+
+    },
+    // ドラッグ中の要素がドロップゾーンに入ったとき
+    // ドラッグ可能な要素がドロップゾーンに到達したことを視覚的に示す
+    ondragenter: function (event) {
+        var dropzoneElement = event.target;
+        dropzoneElement.classList.add('drop-target');
+    },
+    // ドラッグ中の要素がドロップゾーンから離れたとき
+    // ドロップゾーンを離れたことを視覚的に示す
+    ondragleave: function (event) {
+        event.target.classList.remove('drop-target');
+    },
+    // ドラッグ中の要素がドロップゾーンにドロップされたとき
+    // ドロップが成功したことを示す
+    ondrop: function (event) {
+        event.relatedTarget.classList.add('success');
+    },
+    // ドロップ操作が完了したとき
+    // ドロップゾーンのアクティブ状態を解除する
+    ondropdeactivate: function (event) {
+        event.target.classList.remove('drop-active');
+        event.target.classList.remove('drop-target');
+
+        // ドロップが成功していない場合は、元の位置に戻す
+        if(!event.relatedTarget.classList.contains('success')){
+            event.relatedTarget.style.transform = `translate(0px, 0px)`;
+            event.relatedTarget.setAttribute('data-x', 0);
+            event.relatedTarget.setAttribute('data-y', 0);
+        }
+        CheckAmount();
     }
 });
 
@@ -70,3 +117,13 @@ interact('.drag-drop').draggable({
     autoScroll: true,
     listeners: { move: dragMoveListener }
 });
+
+// 全ての要素がドロップゾーンにドロップされたかどうかを確認する関数
+function CheckAmount(){
+    for (let i = 0; i < dragElements.length; i++) {
+        if(!dragElements[i].classList.contains('success')){
+            return;
+        }
+    }
+    alert('クリアおめでとう！！！');
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,13 @@
         <h1 class="text-center text-xl font-bold absolute z-30 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">Happy Birthday SUZU</h1>
         <img src="{{ asset('storage/img/pic-02.jpeg') }}" alt="pic-02" class="object-cover min-h-screen w-full">
     </section>
-    <img src="{{ asset('storage/img/pic-01.png') }}" alt="pic-01" class="yes-drop drag-drop object-cover w-[30dvw] h-[30dvw]">
-    <img src="{{ asset('storage/img/pic-02.png') }}" alt="pic-02" class="yes-drop drag-drop object-cover w-[30dvw] h-[30dvw]">
+    <img src="{{ asset('storage/img/pic-01.png') }}" alt="pic-01" class="vegetable yes-drop drag-drop object-cover w-[30dvw] h-[30dvw]">
+    <img src="{{ asset('storage/img/pic-02.png') }}" alt="pic-02" class="fruit yes-drop drag-drop object-cover w-[30dvw] h-[30dvw]">
 
 
-    <div id="inner-dropzone" class="dropzone">ドロップエリア</div>
+    <div class="flex gap-6">
+        <div id="vegetable_dropzone" class="vegetable_dropzone dropzone">野菜エリア</div>
+        <div id="fruit-dropzone" class="fruit_dropzone dropzone">フルーツエリア</div>
+    </div>
+
 </x-template>
